e50c64ac1a8ff8b9b37a14ef5072ab1e02ec8f08
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-08-05 20:13:20 +0700

    add files

---
+++ server.js
@@ -0,0 +1,18 @@
+/* Setup Sederhana */
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const cors = require('cors')
+const port = process.env.PORT || 7777
+
+// perlu ditambahkan setup file config.env
+
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+// perlu ditambahkan setup file public access
+
+app.listen(port, () => console.log(`server is running in port : ${ port }`))
