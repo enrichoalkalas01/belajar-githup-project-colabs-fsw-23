cff660b68291b8bdd86abe247b4c437e77d28127
Faiz Muttaqin <muttaqinfaiz@gmail.com>
2022-08-05 20:27:45 +0700

    input kedua faiz

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const port = process.env.PORT || 7777;
 
 // perlu ditambahkan setup file config.env
-dotenv.config({ path: __dirname + '/config/config.env' })
+dotenv.config({ path: __dirname + "/config/config.env" });
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -16,5 +16,7 @@ app.set("view engine", "ejs");
 
 // perlu ditambahkan setup file public access
 app.use(express.static("public"));
+
+console.log("Hallo semua");
 
 app.listen(port, () => console.log(`server is running in port : ${port}`));
